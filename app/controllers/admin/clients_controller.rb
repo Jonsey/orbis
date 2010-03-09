3a257839e548c84089ba492a1c31738f8ba20e34
Damian Jones <damianajones@yahoo.co.uk>
2010-03-09 23:44:57 +0000

    Create new client for staff member
    Signed-off-by: Damian Jones <damianajones@yahoo.co.uk>

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -3,6 +3,22 @@ class Admin::ClientsController < ApplicationController
     @clients = Client.all
   end
 
+ def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(params[:client])
+    @client.user_groups << UserGroup.find_by_name('Clients')
+    if @client.save
+      add_lockdown_session_values
+      flash[:notice] = "Account created.."
+      redirect_to admin_clients_path
+    else
+      render :action => 'new'
+    end
+  end
+
   def edit
     @client = Client.find_by_id(params[:id])
   end
