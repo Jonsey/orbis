32bb50afad29d79ce835f3b167203a183969d59b
Damian Jones <damianajones@yahoo.co.uk>
2010-03-18 09:36:19 +0000

    Restructuring applications lists by user type
    Signed-off-by: Damian Jones <damianajones@yahoo.co.uk>

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,6 +1,11 @@
 class Admin::ApplicationsController < ApplicationController
   def index
-    @vacancies = Vacancy.all
+    if current_user.is_a?(Candidate)
+      @status = "live"
+      @vacancies = current_user.vacancies
+    else
+      @vacancies = Vacancy.all
+    end
   end
 
   def show
